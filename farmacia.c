500c84337f5f9d2de6a3e57ab6bc6229d6c353d5
Elyalgoritmos <helen.benalcazar@udla.edu.ec>
2025-10-24 12:56:53 -0500

    Update comments formatting in farmacia.c

--- farmacia.c
+++ farmacia.c
@@ -1,9 +1,9 @@
-//Una farmacia necesita registrar la información de 5 medicamentos.
-//Por cada medicamento se debe guardar:
-//Su nombre
-//Su precio
-//La cantidad disponible
-//El programa debe mostrar al final:
-//Cuántos medicamentos tienen menos de 10 unidades.
-//El valor total del inventario (precio × cantidad).
+//Una farmacia necesita registrar la información de 5 medicamentos.//
+//Por cada medicamento se debe guardar://
+//Su nombre//
+//Su precio//
+//La cantidad disponible//
+//El programa debe mostrar al final://
+//Cuántos medicamentos tienen menos de 10 unidades.//
+//El valor total del inventario (precio × cantidad).//
 //Además, debe indicar si el inventario total supera los 1000 dólares.//
