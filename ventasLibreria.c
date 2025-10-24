f8dc5f0195dacb05078ff14df50344093ad6bc4b
Elyalgoritmos <helen.benalcazar@udla.edu.ec>
2025-10-24 12:56:05 -0500

    Add initial structure for book sales control program

--- ventasLibreria.c
+++ ventasLibreria.c
@@ -1,3 +1,24 @@
+//Control de ventas en una librería
+//Una librería desea implementar un programa en C que le permita llevar el control de las// 
+//ventas de sus 5 libros más vendidos. El sistema debe permitir registrar los datos de cada //
+//libro, calcular el subtotal por venta y mostrar al final el total de dinero recaudado.//
+//Requerimientos del programa//
+//1. Para cada libro se debe ingresar://
+//o ID del libro (número entero).//
+//o Cantidad disponible en stock.//
+//o Precio unitario.//
+//o Cantidad vendida.//
+//2. El programa debe validar que://
+//o Todos los valores ingresados sean números válidos (no letras ni símbolos).//
+//o Las cantidades ingresadas sean mayores o iguales a cero.//
+//o La cantidad vendida no supere el stock disponible.//
+//3. Si la cantidad vendida supera el stock, el programa debe mostrar un mensaje de //
+//error y finalizar inmediatamente.//
+//4. Si los datos son correctos, se debe calcular://
+//o El subtotal de venta por cada libro (precio × cantidad vendida).//
+//5. Al finalizar, el programa debe mostrar://
+//o El subtotal de cada libro vendido.//
+//o El total general recaudado por todas las ventas.//
 #include<studio.h>
 
 main
